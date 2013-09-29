1504fedeb139fab783b5151f03ad13045878e132
tminglei <tmlneu@gmail.com>
2013-09-29 22:18:23 +0800

    jump to v0.1.5

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object SlickPgBuild extends Build {
   lazy val theSettings = Seq(
     name := "slick-pg",
     description := "Slick extensions for PostgreSQL",
-    version := "0.1.3.1",
+    version := "0.1.5",
     organizationName := "slick-pg",
     organization := "com.github.tminglei",
 
