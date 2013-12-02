a57360f04b6c4e454bfbfd41b441c5b01f3026b4
hassan <h.a.syed@gmail.com>
2013-12-02 18:32:21 +0000

    Tokenizer to break up postgres composite / array returns.

---
+++ core/src/main/scala/com/github/tminglei/slickpg/utils/PGObjectTokenizer.scala
@@ -0,0 +1,120 @@
+import scala.collection.mutable
+import scala.util.parsing.combinator.RegexParsers
+
+object PGObjectTokenizer extends RegexParsers {
+  trait Token
+  case class Comma() extends Token
+  case class Chunk(value : String) extends Token
+
+  case class ArrOpen(marker: String,l: Int) extends Token
+  case class RecOpen(marker: String,l: Int) extends Token
+  case class ArrClose(marker: String,l: Int) extends Token
+  case class RecClose(marker: String,l: Int) extends Token
+  case class Escape(l:Int, escape: String) extends Token
+  case class Marker(m : String, l: Int) extends Token
+  case class SingleQuote() extends Token
+  case class Quote(q: String) extends Token
+
+  trait CompositeToken extends Token {
+    def value: List[Token]
+  }
+  case class CTArray(value: List[Token]) extends CompositeToken
+  case class CTRecord(value: List[Token]) extends CompositeToken
+  case class CTString(value: List[Token]) extends CompositeToken
+
+  var level = -1
+  var levelMarker = new mutable.Stack[String]()
+
+  def markerRe = "[\\\\|\"]+".r
+
+  def open: Parser[Token] = opt(markerRe) ~ (elem('{') | elem('(')) ^^ { case(x ~ y) =>
+    val marker_ = x.getOrElse("")
+    levelMarker.push(marker_)
+    val r = y match {
+      case '{' => RecOpen(marker_,level)
+      case '(' => ArrOpen(marker_,level)
+    } ; level += 1; r }
+
+  def close: Parser[Token] = (elem('}') | elem(')')) ~ opt(markerRe) ^^ { case (x~ y) =>
+    level -= 1 ; levelMarker.pop()
+    x match {
+      case '}' => RecClose(y.getOrElse(""),level)
+      case ')' => ArrClose(y.getOrElse(""),level)
+    } }
+
+  def escape = """\\+[^"\\]""".r ^^ {x => Escape(level,"\\" + x.last)}
+
+  def marker =  markerRe ^^ { x=> {
+    val pow = scala.math.pow(2,level).toInt
+    if(x.length % pow == 0) {
+      x.length / pow match {
+        case 1 => SingleQuote()
+        case _ => Quote("\"" * (x.length/pow))
+      }
+
+    }
+    else {
+      Marker(x,level)
+    }
+  } }
+
+  def comma = elem(',') ^^ { x=> Comma() }
+  def chunk = """[^}){(\\,"]+""".r ^^ { Chunk}
+  def tokens = open | close | escape | marker | comma | chunk
+
+  def tokenise = rep(tokens)  ^^ { reduce }
+
+
+  def reduce(tokens : List[Token]): Token = {
+    def isOpen(token: Token) = { token match {
+      case ArrOpen(_,_) => true
+      case RecOpen(_,_) => true
+      case SingleQuote() => true
+      case _ => false
+    } }
+
+    def close(borderToken: Token, source: List[Token], target : List[Token],depth: Int = 0,consumeCount: Int =1): (Token,Int) = {
+      source match {
+        case List() => throw new Exception("reduction should never hit recursive empty list base case.")
+        case x :: xs =>
+          x match {
+            // CLOSING CASES
+            case ArrClose(cm,cl) => borderToken match {
+              case ArrOpen(sm,sl) if cm == sm && sl==cl => (CTArray(target :+ x),consumeCount + 1)
+              case _ => throw new Exception (s"open and close tags don't match : $borderToken - $x")
+            }
+            case RecClose(cm,cl) => borderToken match {
+              case RecOpen(sm,sl) if cm == sm && sl == cl => (CTRecord(target :+ x),consumeCount + 1)
+              case _ => throw new Exception (s"open and close tags don't match:  : $borderToken - $x")
+            }
+            case SingleQuote() if borderToken.isInstanceOf[SingleQuote] => (CTString(target :+ x ),consumeCount +1)
+            // the else porting of this should be caught by the isOpen case below
+            // OPENING CASES -> The results of these are siblings.
+            case xx if isOpen(x) => {
+              val (sibling, consumed) = { val ret = close(x,xs,x::List(),depth +1); (ret._1.asInstanceOf[CompositeToken], ret._2) }
+              val new_source =  source.splitAt(consumed)._2
+              close(borderToken,new_source,target :+ sibling,consumeCount = consumeCount + consumed)
+            }
+            case _ => close(borderToken, xs, target :+ x, consumeCount = consumeCount +1)
+          }
+      }
+    }
+
+    tokens match {
+      case x :: xs if xs != List() =>
+        val ret =
+        x match {
+          case xx if isOpen(x)  => close(x, xs, x :: List() )
+          case _ => throw new Exception("open must always deal with a open token")
+        }
+        if(ret._2 != tokens.size)
+          throw new Exception("reduction step did not cover all tokens.")
+        ret._1
+    }
+  }
+  def apply(input : String) = {
+    level = -1
+    levelMarker.clear()
+    parseAll(tokenise,new scala.util.parsing.input.CharArrayReader(input.toCharArray))
+  }
+}
