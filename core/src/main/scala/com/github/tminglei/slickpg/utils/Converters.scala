93d700264f6d4c249bafd19638ba44a9111c6378
tminglei <tmlneu@gmail.com>
2013-12-05 10:36:15 +0800

    adding composite type support (init)

---
+++ core/src/main/scala/com/github/tminglei/slickpg/utils/Converters.scala
@@ -0,0 +1,180 @@
+package com.github.tminglei.slickpg.utils
+
+import scala.reflect.runtime.{universe => ru, currentMirror => runtimeMirror}
+import scala.reflect.ClassTag
+
+object Converters {
+  @scala.annotation.implicitNotFound(msg = "No converter available for ${FROM} to ${TO}")
+  trait Converter[FROM, TO] extends (FROM => TO)
+
+  object Converter {
+    def apply[FROM, TO](convert: (FROM => TO)) =
+      new Converter[FROM, TO] {
+        def apply(v: FROM) = convert(v)
+      }
+  }
+
+  ///
+  private[utils] case class CacheKey(val from: ru.Type, val to: ru.Type) {
+    override def equals(o: Any) = {
+      if (o.isInstanceOf[CacheKey]) {
+        val that = o.asInstanceOf[CacheKey]
+        from =:= that.from && to =:= that.to
+      } else false
+    }
+  }
+
+  private var converterMap = Map[CacheKey, Converter[_, _]]()
+
+  private[utils] def internalGet(from: ru.Type, to: ru.Type) = {
+    val cacheKey = CacheKey(from, to)
+    converterMap.get(cacheKey).orElse({
+      if (to <:< from) {
+        converterMap += (cacheKey -> Converter((v: Any) => v))
+        converterMap.get(cacheKey)
+      } else None
+    })
+  }
+
+  def register[FROM,TO](convert: (FROM => TO))(implicit from: ru.TypeTag[FROM], to: ru.TypeTag[TO]) = {
+    println(s"register converter for ${from.tpe} => ${to.tpe}")
+    converterMap += (CacheKey(from.tpe, to.tpe) -> Converter(convert))
+  }
+
+  def converter[FROM,TO](implicit from: ru.TypeTag[FROM], to: ru.TypeTag[TO]): Converter[FROM,TO] = {
+    internalGet(from.tpe, to.tpe).map(_.asInstanceOf[Converter[FROM,TO]])
+      .getOrElse(throw new IllegalArgumentException(s"Converter NOT FOUND for ${from.tpe} => ${to.tpe}"))
+  }
+
+  ////////////////////////////////////////////////////////////////
+  object Util {
+    import PGObjectTokenizer.PGElements._
+
+    //
+    private val strType  = ru.typeOf[String]
+    private val elemType = ru.typeOf[Element]
+
+    private def convertToValue(e: Element, toType: ru.Type): Any = e match {
+      case ValueE(v)  => Converters.internalGet(strType, toType).get.asInstanceOf[Converter[String,_]](v)
+      case _: CompositeE  => Converters.internalGet(elemType, toType).get.asInstanceOf[Converter[Element,_]](e)
+      case ArrayE(el) => {
+        val eType = toType.asInstanceOf[ru.TypeRef].args(0)
+        el.map(e => convertToValue(e, eType))
+      }
+    }
+
+    private def convertToElement(v: Any, fromType: ru.Type): Element = {
+      (v, fromType) match {
+        case (Some(realVal), _) => {
+          val realType = fromType.asInstanceOf[ru.TypeRef].args(0)
+          convertToElement(realVal, realType)
+        }
+        case (vList: List[_], _) => {
+          val elemType = fromType.asInstanceOf[ru.TypeRef].args(0)
+          ArrayE(vList.map(convertToElement(_, elemType)))
+        }
+        case (_, _) => {
+          Converters.internalGet(fromType, elemType)
+            .map(_.asInstanceOf[Converter[Any, Element]](v))
+            .getOrElse(if (v == null || v == None) null else ValueE(v.toString) )
+        }
+      }
+    }
+
+    ///
+    def mkConvFromElement[T <: AnyRef](implicit ev: ru.TypeTag[T]): Converter[Element, T] = {
+
+      new Converter[Element, T] {
+        private val thisType = ru.typeOf[T]
+        private val optType = ru.typeOf[Option[_]]
+
+        private val constructor = thisType.declaration(ru.nme.CONSTRUCTOR).asMethod
+        private val ctorArgInfo = constructor.paramss.head.map(_.typeSignature).map(tpe =>
+          if (tpe.typeConstructor =:= optType.typeConstructor) {
+            val realType = tpe.asInstanceOf[ru.TypeRef].args(0)
+            (realType, true)
+          } else (tpe, false)
+        )
+
+        //--
+        def apply(elem: Element): T = {
+          val classMirror = runtimeMirror.reflectClass(thisType.typeSymbol.asClass)
+          val ctorMirror = classMirror.reflectConstructor(constructor)
+
+          val args = elem.asInstanceOf[CompositeE].members.zip(ctorArgInfo).map {
+            case (e, (tpe, isOption)) => {
+              val tv = convertToValue(e, tpe)
+              if (isOption) Option(tv) else tv
+            }
+          }
+          ctorMirror.apply(args: _*).asInstanceOf[T]
+        }
+      }
+    }
+
+    def mkArrayConvFromElement[T](implicit ev: ru.TypeTag[T]): Converter[Element, List[T]] = {
+
+      new Converter[Element, List[T]] {
+        private val thisType = ru.typeOf[T]
+        private val optType = ru.typeOf[Option[_]]
+
+        private val (realType, isOption) =
+          if (thisType.typeConstructor =:= optType.typeConstructor) {
+            val realType = thisType.asInstanceOf[ru.TypeRef].args(0)
+            (realType, true)
+          } else (thisType, false)
+
+        //--
+        def apply(elem: Element): List[T] = {
+          elem.asInstanceOf[ArrayE].elements.map { e =>
+            val tv = convertToValue(e, realType)
+            if (isOption) Option(tv) else tv
+          } map (_.asInstanceOf[T])
+        }
+      }
+    }
+
+    ////
+    def mkConvToElement[T](implicit ev: ru.TypeTag[T], ev1: ClassTag[T]): Converter[T, Element] = {
+
+      new Converter[T, Element] {
+        private val thisType = ru.typeOf[T]
+
+        private val constructor = thisType.declaration(ru.nme.CONSTRUCTOR).asMethod
+        private val ctorFieldInfo = constructor.paramss.head.map(t => {
+          val field = thisType.declaration(t.name).asTerm
+          val tpe  = t.typeSignature
+          (field, tpe)
+        })
+
+        //--
+        def apply(v: T): Element = {
+          val instanceMirror = runtimeMirror.reflect(v)
+          CompositeE(
+            ctorFieldInfo.map {
+              case (field, tpe) => {
+                val fv = instanceMirror.reflectField(field).get
+                convertToElement(fv, tpe)
+              }
+            }
+          )
+        }
+      }
+    }
+
+    def mkArrayConvToElement[T](implicit ev: ru.TypeTag[T], ev1: ClassTag[T]): Converter[List[T], Element] = {
+
+      new Converter[List[T], Element] {
+        private val thisType = ru.typeOf[T]
+
+        def apply(vList: List[T]): Element = {
+          ArrayE(
+            vList.map { v =>
+              convertToElement(v, thisType)
+            }
+          )
+        }
+      }
+    }
+  }
+}
--- core/src/main/scala/com/github/tminglei/slickpg/utils/PGObjectTokenizer.scala
+++ core/src/main/scala/com/github/tminglei/slickpg/utils/PGObjectTokenizer.scala
@@ -1,123 +1,135 @@
+package com.github.tminglei.slickpg.utils
+
 import scala.collection.mutable
 import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.input.CharSequenceReader
+import scala.slick.SlickException
 
-sealed trait PGTokens
-{
-  trait Token
-  case class Comma()                          extends Token
+class PGObjectTokenizer extends RegexParsers {
 
-  case class ArrOpen(marker: String,l: Int)   extends Token
-  case class RecOpen(marker: String,l: Int)   extends Token
-  case class ArrClose(marker: String,l: Int)  extends Token
-  case class RecClose(marker: String,l: Int)  extends Token
-  case class Escape(l:Int, escape: String)    extends Token
-  case class Marker(m : String, l: Int)       extends Token
-  case class SingleQuote()                    extends Token
+  // pg tokens, should be used internally only
+  object PGTokens {
+    sealed trait Token
+    case class Comma()                          extends Token
 
+    case class ArrOpen(marker: String,l: Int)   extends Token
+    case class RecOpen(marker: String,l: Int)   extends Token
+    case class ArrClose(marker: String,l: Int)  extends Token
+    case class RecClose(marker: String,l: Int)  extends Token
+    case class Escape(l:Int, escape: String)    extends Token
+    case class Marker(m : String, l: Int)       extends Token
+    case class SingleQuote()                    extends Token
 
-  trait ValueToken extends Token {
-    def value: String
+    trait ValueToken extends Token {
+      def value: String
+    }
+    case class Chunk(value : String)            extends ValueToken
+    case class Quote(value: String)             extends ValueToken
+
+    trait CompositeToken extends Token {
+      def value: List[Token]
+    }
+    case class CTArray(value: List[Token])      extends CompositeToken
+    case class CTRecord(value: List[Token])     extends CompositeToken
+    case class CTString(value: List[Token])     extends CompositeToken
   }
-  case class Chunk(value : String)            extends ValueToken
-  case class Quote(value: String)             extends ValueToken
 
-  trait CompositeToken extends Token {
-    def value: List[Token]
-  }
-  case class CTArray(value: List[Token])      extends CompositeToken
-  case class CTRecord(value: List[Token])     extends CompositeToken
-  case class CTString(value: List[Token])     extends CompositeToken
-}
+  ////////////////////////////////////
+  import PGTokens._
+  import PGObjectTokenizer.PGElements._
 
-sealed trait PGElements {
-  sealed trait Element
-  case class ValueE(value: String) extends Element
-  case class ArrayE(elements: List[Element]) extends Element
-  case class CompositeE(members: List[Element]) extends Element
-}
+  object PGTokenReducer {
 
-sealed trait PGTokenReducer extends PGTokens with PGElements {
-  def compose(input : CompositeToken) : Element =  {
-    def mergeString(list : List[Token]) : String = {
-      if(list.isEmpty) ""
-      else
-        list.head match {
-          case Chunk(v) => v + mergeString(list.tail)
-          case Quote(v) => v + mergeString(list.tail)
-          case Escape(_,v) => v + mergeString(list.tail)
-          case Comma() => mergeString(list.tail)
+    def compose(input : CompositeToken) : Element =  {
+      
+      def mergeString(list : List[Token]) : String = {
+        if(list.isEmpty) ""
+        else
+          list.head match {
+            case Chunk(v) => v + mergeString(list.tail)
+            case Quote(v) => v + mergeString(list.tail)
+            case Escape(_,v) => v + mergeString(list.tail)
+            case Comma() => mergeString(list.tail)
+            case token => throw new IllegalArgumentException(s"unsupported token $token")
+          }
+      }
+
+      def mergeComposite(composite :CompositeToken, level: Int = 0) : Element = {
+        val elements =
+          composite.value.collect {
+            case v: CTArray   => mergeComposite(v)
+            case v: CTRecord  => mergeComposite(v)
+            case CTString(v)  => ValueE(mergeString(v.slice(1,v.length-1)))
+            case Chunk(v) => ValueE(v)
+          }
+
+        composite match {
+          case CTArray(_) => ArrayE(elements)
+          case CTRecord(_) => CompositeE(elements)
+          case token => throw new IllegalArgumentException(s"unsupported token $token")
         }
-    }
-    def mergeComposite(composite :CompositeToken, level: Int = 0) : Element = {
-      val elements =
-      composite.value.collect {
-        case v: CTArray   => mergeComposite(v)
-        case v: CTRecord  => mergeComposite(v)
-        case CTString(v)  => ValueE(mergeString(v.slice(1,v.length-1)))
-        case Chunk(v) => ValueE(v)
       }
 
-      composite match {
-        case CTArray(_) => ArrayE(elements)
-        case CTRecord(_) => CompositeE(elements)
-        case _ => throw new Exception("merge composite should only be called for composites.")
+      //--
+      mergeComposite(input)
+    }
+
+    def reduce(tokens : List[Token]): CompositeToken = {
+
+      def isOpen(token: Token) = { token match {
+        case ArrOpen(_,_) => true
+        case RecOpen(_,_) => true
+        case SingleQuote() => true
+        case _ => false
+      } }
+
+      def close(borderToken: Token, source: List[Token], target : List[Token],depth: Int = 0,consumeCount: Int =1): (CompositeToken,Int) = {
+        source match {
+          case List() => throw new Exception("reduction should never hit recursive empty list base case.")
+          case x :: xs =>
+            x match {
+              // CLOSING CASES
+              case ArrClose(cm,cl) => borderToken match {
+                case ArrOpen(sm,sl) if cm == sm && sl==cl => (CTArray(target :+ x),consumeCount + 1)
+                case _ => throw new Exception (s"open and close tags don't match : $borderToken - $x")
+              }
+              case RecClose(cm,cl) => borderToken match {
+                case RecOpen(sm,sl) if cm == sm && sl == cl => (CTRecord(target :+ x),consumeCount + 1)
+                case _ => throw new Exception (s"open and close tags don't match:  : $borderToken - $x")
+              }
+              case SingleQuote() if borderToken.isInstanceOf[SingleQuote] => (CTString(target :+ x ),consumeCount +1)
+              // the else porting of this should be caught by the isOpen case below
+              // OPENING CASES -> The results of these are siblings.
+              case xx if isOpen(x) => {
+                val (sibling, consumed) = { val ret = close(x,xs,x::List(),depth +1); (ret._1.asInstanceOf[CompositeToken], ret._2) }
+                val new_source =  source.splitAt(consumed)._2
+                close(borderToken,new_source,target :+ sibling,consumeCount = consumeCount + consumed)
+              }
+              case _ => close(borderToken, xs, target :+ x, consumeCount = consumeCount +1)
+            }
+        }
       }
 
+      //--
+      tokens match {
+        case x :: xs if xs != List() =>
+          val ret =
+            x match {
+              case xx if isOpen(x)  => close(x, xs, x :: List() )
+              case _ => throw new Exception("open must always deal with an open token.")
+            }
+          if(ret._2 != tokens.size)
+            throw new Exception("reduction step did not cover all tokens.")
+          ret._1
+      }
     }
-    mergeComposite(input)
   }
 
-  def reduce(tokens : List[Token]): CompositeToken = {
-    def isOpen(token: Token) = { token match {
-      case ArrOpen(_,_) => true
-      case RecOpen(_,_) => true
-      case SingleQuote() => true
-      case _ => false
-    } }
-
-    def close(borderToken: Token, source: List[Token], target : List[Token],depth: Int = 0,consumeCount: Int =1): (CompositeToken,Int) = {
-      source match {
-        case List() => throw new Exception("reduction should never hit recursive empty list base case.")
-        case x :: xs =>
-          x match {
-            // CLOSING CASES
-            case ArrClose(cm,cl) => borderToken match {
-              case ArrOpen(sm,sl) if cm == sm && sl==cl => (CTArray(target :+ x),consumeCount + 1)
-              case _ => throw new Exception (s"open and close tags don't match : $borderToken - $x")
-            }
-            case RecClose(cm,cl) => borderToken match {
-              case RecOpen(sm,sl) if cm == sm && sl == cl => (CTRecord(target :+ x),consumeCount + 1)
-              case _ => throw new Exception (s"open and close tags don't match:  : $borderToken - $x")
-            }
-            case SingleQuote() if borderToken.isInstanceOf[SingleQuote] => (CTString(target :+ x ),consumeCount +1)
-            // the else porting of this should be caught by the isOpen case below
-            // OPENING CASES -> The results of these are siblings.
-            case xx if isOpen(x) => {
-              val (sibling, consumed) = { val ret = close(x,xs,x::List(),depth +1); (ret._1.asInstanceOf[CompositeToken], ret._2) }
-              val new_source =  source.splitAt(consumed)._2
-              close(borderToken,new_source,target :+ sibling,consumeCount = consumeCount + consumed)
-            }
-            case _ => close(borderToken, xs, target :+ x, consumeCount = consumeCount +1)
-          }
-      }
-    }
-
-    tokens match {
-      case x :: xs if xs != List() =>
-        val ret =
-          x match {
-            case xx if isOpen(x)  => close(x, xs, x :: List() )
-            case _ => throw new Exception("open must always deal with an open token.")
-          }
-        if(ret._2 != tokens.size)
-          throw new Exception("reduction step did not cover all tokens.")
-        ret._1
-    }
-  }
-}
-
-object PGObjectTokenizer extends RegexParsers with PGTokens with PGTokenReducer {
+  ////////////////////////////////////////////////////////////////////////////////
+  import PGTokenReducer._
+  
   override def skipWhitespace = false
+  
   var level = -1
   var levelMarker = new mutable.Stack[String]()
 
@@ -127,45 +139,57 @@ object PGObjectTokenizer extends RegexParsers with PGTokens with PGTokenReducer 
     val marker_ = x.getOrElse("")
     levelMarker.push(marker_)
     val r = y match {
-      case '{' => RecOpen(marker_,level)
-      case '(' => ArrOpen(marker_,level)
+      case '{' => ArrOpen(marker_,level)
+      case '(' => RecOpen(marker_,level)
     } ; level += 1; r }
 
   def close: Parser[Token] = (elem('}') | elem(')')) ~ opt(markerRe) ^^ { case (x~ y) =>
     level -= 1 ; levelMarker.pop()
     x match {
-      case '}' => RecClose(y.getOrElse(""),level)
-      case ')' => ArrClose(y.getOrElse(""),level)
+      case '}' => ArrClose(y.getOrElse(""),level)
+      case ')' => RecClose(y.getOrElse(""),level)
     } }
 
   def escape = """\\+[^"\\]""".r ^^ {x => Escape(level,"\\" + x.last)}
 
-  def marker =  markerRe ^^ { x=> {
+  def marker =  markerRe ^^ { x=>
     val pow = scala.math.pow(2,level).toInt
     if(x.length % pow == 0) {
       x.length / pow match {
         case 1 => SingleQuote()
         case _ => Quote("\"" * (x.length/pow))
       }
-
     }
     else {
       Marker(x,level)
     }
-  } }
+  }
 
   def comma = elem(',') ^^ { x=> Comma() }
   def chunk = """[^}){(\\,"]+""".r ^^ { Chunk}
   def tokens = open | close | escape | marker | comma | chunk
 
-  def tokenise = rep(tokens)  ^^ { t=> compose(reduce(t)) }
+  def tokenize = rep(tokens)  ^^ { t=> compose(reduce(t)) }
 
+  //--
+  def process(input : String) = {
+    parseAll(tokenize, new CharSequenceReader(input))
+  }
+}
 
+object PGObjectTokenizer extends PGObjectTokenizer {
 
-  def apply(input : String) = {
-    level = -1
-    levelMarker.clear()
-    println(input)
-    parseAll(tokenise,new scala.util.parsing.input.CharArrayReader(input.toCharArray))
+  object PGElements {
+    sealed trait Element
+    case class ValueE(value: String) extends Element
+    case class ArrayE(elements: List[Element]) extends Element
+    case class CompositeE(members: List[Element]) extends Element
+  }
+
+  def apply(input : String): PGElements.Element = {
+    new PGObjectTokenizer().process(input) match {
+      case Success(result, _) => result
+      case failure: NoSuccess => throw new SlickException(failure.msg)
+    }
   }
 }
