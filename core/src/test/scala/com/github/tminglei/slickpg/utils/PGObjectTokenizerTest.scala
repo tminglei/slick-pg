aae7ae8c4893ed138ce4db8ab86ea3c25e8d8ba3
tminglei <tmlneu@gmail.com>
2014-01-06 18:50:51 +0800

    PGObjectTokenizer - support nested range type

--- core/src/test/scala/com/github/tminglei/slickpg/utils/PGObjectTokenizerTest.scala
+++ core/src/test/scala/com/github/tminglei/slickpg/utils/PGObjectTokenizerTest.scala
@@ -9,13 +9,13 @@ class PGObjectTokenizerTest {
 
   @Test
   def testSimpleComposite(): Unit = {
-    val input = """(111,test,"test desc",hi)"""
+    val input = """(111,test,"test desc","2010-01-01 14:30")"""
     val expected =
       CompositeE(List(
         ValueE("111"),
         ValueE("test"),
         ValueE("test desc"),
-        ValueE("hi")
+        ValueE("2010-01-01 14:30")
       ))
 
     assertEquals(expected, PGObjectTokenizer.tokenize(input))
@@ -61,8 +61,8 @@ class PGObjectTokenizerTest {
 
   @Test
   def testSimpleNestedComposite(): Unit = {
-    val input = """(115,"(111,test,""test dd"",hi)",{157})"""
-    val input_v = """(115,"(111,test,""test dd"",hi)","{157}")"""
+    val input = """(115,"(111,test,""test dd"",""2010-01-01 14:30"")",{157})"""
+    val input_v = """(115,"(111,test,""test dd"",""2010-01-01 14:30"")","{157}")"""
     val expected =
       CompositeE(List(
         ValueE("115"),
@@ -70,7 +70,7 @@ class PGObjectTokenizerTest {
           ValueE("111"),
           ValueE("test"),
           ValueE("test dd"),
-          ValueE("hi"))),
+          ValueE("2010-01-01 14:30"))),
         ArrayE(List(
           ValueE("157")
         ))
@@ -79,11 +79,11 @@ class PGObjectTokenizerTest {
     assertEquals(expected, PGObjectTokenizer.tokenize(input))
     assertEquals(input_v, PGObjectTokenizer.reverse(expected))
 
-    val input01 = """(115,"(111,""test"",""test dd"",hi)",{157})"""
+    val input01 = """(115,"(111,""test"",""test dd"",""2010-01-01 14:30"")",{157})"""
     assertEquals(expected, PGObjectTokenizer.tokenize(input01))
 
     ///
-    val input1 = """(115,"(111,test,""test dd"",hi)",)"""
+    val input1 = """(115,"(111,test,""test dd"",""2010-01-01 14:30"")",)"""
     val expected1 =
       CompositeE(List(
         ValueE("115"),
@@ -91,7 +91,7 @@ class PGObjectTokenizerTest {
           ValueE("111"),
           ValueE("test"),
           ValueE("test dd"),
-          ValueE("hi"))),
+          ValueE("2010-01-01 14:30"))),
         NullE
       ))
 
@@ -138,8 +138,8 @@ class PGObjectTokenizerTest {
 
   @Test
   def testNestedArrayComposite(): Unit = {
-    val input = """(115,"{""(111,test,\\""test desc\\"",hi)""}",{157})"""
-    val input_v = """(115,"{""(111,test,\\""test desc\\"",hi)""}","{157}")"""
+    val input = """(115,"{""(111,test,\\""test desc\\"",\\""2010-01-01 14:30\\"")""}",{157})"""
+    val input_v = """(115,"{""(111,test,\\""test desc\\"",\\""2010-01-01 14:30\\"")""}","{157}")"""
     val expected =
       CompositeE(List(
         ValueE("115"),
@@ -148,7 +148,7 @@ class PGObjectTokenizerTest {
             ValueE("111"),
             ValueE("test"),
             ValueE("test desc"),
-            ValueE("hi")
+            ValueE("2010-01-01 14:30")
           ))
         )),
         ArrayE(List(
@@ -162,8 +162,8 @@ class PGObjectTokenizerTest {
 
   @Test
   def testComplexNestedComposite(): Unit = {
-    val input = """(115,"{""(111,test,\\""(103,ttt)\\"",hi)""}",{157})"""
-    val input_v = """(115,"{""(111,test,\\""(103,ttt)\\"",hi)""}","{157}")"""
+    val input = """(115,"{""(111,test,\\""(103,ttt)\\"",\\""2010-01-01 14:30\\"")""}",{157})"""
+    val input_v = """(115,"{""(111,test,\\""(103,ttt)\\"",\\""2010-01-01 14:30\\"")""}","{157}")"""
     val expected =
       CompositeE(List(
         ValueE("115"),
@@ -175,7 +175,7 @@ class PGObjectTokenizerTest {
               ValueE("103"),
               ValueE("ttt")
             )),
-            ValueE("hi")
+            ValueE("2010-01-01 14:30")
           ))
         )),
         ArrayE(List(
@@ -187,47 +187,16 @@ class PGObjectTokenizerTest {
     assertEquals(input_v, PGObjectTokenizer.reverse(expected))
 
     ///
-    val input1 = """(115,"{""(111,test,\\""(103,ttt,\\""\\""{\\""\\""\\""\\""(131,\\\\\\\\\\\\\\\\\\""\\""\\""\\""tl k\\\\\\\\\\\\\\\\\\""\\""\\""\\"")\\""\\""\\""\\""}\\""\\"")\\"",hi)""}",{157})"""
-    val input1v = """(115,"{""(111,test,\\""(103,ttt,\\\\\\""{\\\\\\\\\\\\\\""(131,\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\""tl k\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"")\\\\\\\\\\\\\\""}\\\\\\"")\\"",hi)""}","{157}")"""
-    val expected1 = //row(115,array[row(111,'test',row(103,'ttt',array[row(131,'tl k')]),'hi')],array[157]);
+    val input1 = """(&#40;t4,"(""&#92;t3"",""{""\""(&#123;t2,\\\\""\""{\\\\""\""\\\\""\""(t1,\\\\\\\\\\\\\\\\\\\\""\""\\\\""\""{99,13}\\\\\\\\\\\\\\\\\\\\""\""\\\\""\"")\\\\""\""\\\\""\""}\\\\""\"")""\""}"")")"""
+    val input1v = """("&#40;t4","(""&#92;t3"",""{\\""(&#123;t2,\\\\\\""{\\\\\\\\\\\\\\""(t1,\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\""{99,13}\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"")\\\\\\\\\\\\\\""}\\\\\\"")\\""}"")")"""
+    val expected1 = //row('(t4', row('\t3', array[row('{t2', array[row('t1', array[99,13])])]));
       CompositeE(List(
-        ValueE("115"),
-        ArrayE(List(
-          CompositeE(List(
-            ValueE("111"),
-            ValueE("test"),
-            CompositeE(List(
-              ValueE("103"),
-              ValueE("ttt"),
-              ArrayE(List(
-                CompositeE(List(
-                  ValueE("131"),
-                  ValueE("tl k")
-                ))
-              ))
-            )),
-            ValueE("hi")
-          ))
-        )),
-        ArrayE(List(
-          ValueE("157")
-        ))
-      ))
-
-    assertEquals(expected1, PGObjectTokenizer.tokenize(input1))
-    assertEquals(input1v, PGObjectTokenizer.reverse(expected1))
-
-    ///
-    val input2 = """(t4,"(t3,""{""\""(t2,\\\\""\""{\\\\""\""\\\\""\""(t1,\\\\\\\\\\\\\\\\\\\\""\""\\\\""\""{99,13}\\\\\\\\\\\\\\\\\\\\""\""\\\\""\"")\\\\""\""\\\\""\""}\\\\""\"")""\""}"")")"""
-    val input2v = """(t4,"(t3,""{\\""(t2,\\\\\\""{\\\\\\\\\\\\\\""(t1,\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\""{99,13}\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"")\\\\\\\\\\\\\\""}\\\\\\"")\\""}"")")"""
-    val expected2 = //row('t4', row('t3', array[row('t2', array[row('t1', array[99,13])])]));
-      CompositeE(List(
-        ValueE("t4"),
+        ValueE("(t4"),
         CompositeE(List(
-          ValueE("t3"),
+          ValueE("\\t3"),
           ArrayE(List(
             CompositeE(List(
-              ValueE("t2"),
+              ValueE("{t2"),
               ArrayE(List(
                 CompositeE(List(
                   ValueE("t1"),
@@ -242,26 +211,48 @@ class PGObjectTokenizerTest {
         ))
       ))
 
+    assertEquals(expected1, PGObjectTokenizer.tokenize(input1))
+    assertEquals(input1v, PGObjectTokenizer.reverse(expected1))
+
+    ///
+    val input2 = """{"(\"t3&#34;s\",\"{\"\"(t2's,\\\\\"\"{\\\\\"\"\\\\\"\"(\\\\\\\\\\\\\\\\\\\\\"\"\\\\\"\"t1 s\\\\\\\\\\\\\\\\\\\\\"\"\\\\\"\",)\\\\\"\"\\\\\"\"}\\\\\"\")\"\"}\")"}"""
+    val input2v = """{"(\"t3&#34;s\",\"{\"\"(t2''s,\\\\\"\"{\\\\\\\\\\\\\"\"(\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\"t1 s\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\",)\\\\\\\\\\\\\"\"}\\\\\"\")\"\"}\")"}"""
+    val expected2 = //array[row('t3"s', array[row('t2''s', array[row('t1,s', null)])])];
+      ArrayE(List(
+        CompositeE(List(
+          ValueE("t3\"s"),
+          ArrayE(List(
+            CompositeE(List(
+              ValueE("t2's"),
+              ArrayE(List(
+                CompositeE(List(
+                  ValueE("t1 s"),
+                  NullE
+                ))
+              ))
+            ))
+          ))
+        ))
+      ))
+
     assertEquals(expected2, PGObjectTokenizer.tokenize(input2))
     assertEquals(input2v, PGObjectTokenizer.reverse(expected2))
 
     ///
-    val input3 = """{"(301,\"{\"\"(201,\\\\\"\"{\\\\\"\"\\\\\"\"(101,\\\\\\\\\\\\\\\\\\\\\"\"\\\\\"\"{99,13}\\\\\\\\\\\\\\\\\\\\\"\"\\\\\"\")\\\\\"\"\\\\\"\"}\\\\\"\")\"\"}\")"}"""
-    val input3v = """{"(301,\"{\"\"(201,\\\\\"\"{\\\\\\\\\\\\\"\"(101,\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\"{99,13}\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\")\\\\\\\\\\\\\"\"}\\\\\"\")\"\"}\")"}"""
-    val expected3 = //array[row(301, array[row(201, array[row(101, array[99,13])])])]
+    val input3 = """{"(t3,\"(t2,\"\"(t1,\"\"\"\"{\"\"\"\"\"\"\"\"(t,\\\\\\\\\\\\\\\\\"\"\"\"\"\"\"\"[2010-01-01 14:30, 2010-01-01 15:30)\\\\\\\\\\\\\\\\\"\"\"\"\"\"\"\")\"\"\"\"\"\"\"\"}\"\"\"\")\"\")\")"}"""
+    val input3v = """{"(t3,\"(t2,\"\"(t1,\\\\\"\"{\\\\\\\\\\\\\"\"(t,\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\"[2010-01-01 14:30, 2010-01-01 15:30)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\")\\\\\\\\\\\\\"\"}\\\\\"\")\"\")\")"}"""
+    val expected3 = //array[row('t3', row('t2', row('t1', array[row('t', '[2010-01-01 14:30, 2010-01-01 15:30)')])))]
       ArrayE(List(
         CompositeE(List(
-          ValueE("301"),
-          ArrayE(List(
+          ValueE("t3"),
+          CompositeE(List(
+            ValueE("t2"),
             CompositeE(List(
-              ValueE("201"),
+              ValueE("t1"),
               ArrayE(List(
                 CompositeE(List(
-                  ValueE("101"),
-                  ArrayE(List(
-                    ValueE("99"),
-                    ValueE("13")
-                  ))
+                  ValueE("t"),
+                  ValueE("[2010-01-01 14:30, 2010-01-01 15:30)")
                 ))
               ))
             ))
@@ -271,5 +262,60 @@ class PGObjectTokenizerTest {
 
     assertEquals(expected3, PGObjectTokenizer.tokenize(input3))
     assertEquals(input3v, PGObjectTokenizer.reverse(expected3))
+  }
+
+  @Test
+  def testPatternsInUsed(): Unit = {
+    val tokenizer = new PGObjectTokenizer
+
+    import tokenizer.PGTokenReverser.{MARK_LETTERS, RANGE_STRING}
+
+    assertTrue(MARK_LETTERS findFirstIn "xxx,tt" isDefined)
+    assertTrue(MARK_LETTERS findFirstIn "\\ttt" isDefined)
+    assertTrue(MARK_LETTERS findFirstIn "tt st" isDefined)
+    assertTrue(MARK_LETTERS findFirstIn "(ttt" isDefined)
+    assertTrue(MARK_LETTERS findFirstIn "ttt)" isDefined)
+
+    assertTrue(RANGE_STRING findFirstIn "[111,234)" isDefined)
+    assertTrue(RANGE_STRING findFirstIn "[2013-11-4, 2014-1-1)" isDefined)
+    assertTrue(RANGE_STRING findFirstIn "[\"2013-11-4\", \"2014-1-1\")" isDefined)
+
+    import tokenizer.PGTokenReducer.{DATE, TIMESTAMP, INTEGER, FLOAT}
+
+    assertTrue(INTEGER findFirstIn "111" isDefined)
+    assertTrue(INTEGER findFirstIn " 111" isEmpty)
+    assertTrue(FLOAT findFirstIn "103.22" isDefined)
+    assertTrue(FLOAT findFirstIn "te 134.1" isEmpty)
+    assertTrue(DATE findFirstIn "2013-11-1" isDefined)
+    assertTrue(DATE findFirstIn "tt 2013-11-01" isEmpty)
+    assertTrue(TIMESTAMP findFirstIn "2010-01-1 14:30" isDefined)
+    assertTrue(TIMESTAMP findFirstIn "2010-01-01T14:30:00" isDefined)
+    assertTrue(TIMESTAMP findFirstIn "tt 2010-01-01 14:30" isEmpty)
+
+    assertTrue(
+      "111" match {
+        case INTEGER() => true
+        case _ => false
+      })
+    assertTrue(
+      "103.22" match {
+        case FLOAT(_) => true
+        case _ => false
+      })
+    assertTrue(
+      "2013-11-01" match {
+        case DATE() => true
+        case _ => false
+      })
+    assertTrue(
+      "2010-01-01 14:30" match {
+        case TIMESTAMP(_,_) => true
+        case _ => false
+      })
+    assertTrue(
+      ("2010-01-01 14:30", "2010-01-01 15:30") match {
+        case (TIMESTAMP(_,_), TIMESTAMP(_,_)) => true
+        case _  => false
+      })
   }
 }
