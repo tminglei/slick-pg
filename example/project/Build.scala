28faa2298403f862f5597d00679c85b8866b4132
tminglei <tmlneu@gmail.com>
2014-01-11 11:36:59 +0800

    update example project and sbt dependency

--- example/project/Build.scala
+++ example/project/Build.scala
@@ -6,11 +6,11 @@ object ExampleBuild extends Build {
   lazy val theSettings = Seq(
     name := "example",
     description := "slick-pg example project",
-    version := "0.2-SNAPSHOT",
+    version := "0.5-SNAPSHOT",
     organizationName := "slick-pg",
     organization := "com.example",
 
-    scalaVersion := "2.10.1",
+    scalaVersion := "2.10.3",
     scalaBinaryVersion <<= scalaVersion,
     scalacOptions ++= Seq("-deprecation", "-feature",
       "-language:implicitConversions",
@@ -18,11 +18,11 @@ object ExampleBuild extends Build {
       "-language:postfixOps"),
 
     libraryDependencies := Seq(
-      "com.typesafe.slick" % "slick_2.10" % "2.0.0-M3",
-      "com.github.tminglei" % "slick-pg_2.10.3" % "0.5.0-beta",
+      "com.typesafe.slick" % "slick_2.10" % "2.0.0-RC1",
+      "com.github.tminglei" % "slick-pg_2.10.3" % "0.5.0-RC1",
       "org.postgresql" % "postgresql" % "9.3-1100-jdbc41",
       "com.vividsolutions" % "jts" % "1.13",
-      "org.json4s" % "json4s-native_2.10" % "3.2.5"
+      "org.json4s" % "json4s-native_2.10" % "3.2.6"
     ),
     resolvers += Resolver.mavenLocal,
     resolvers += Resolver.sonatypeRepo("snapshots"),
