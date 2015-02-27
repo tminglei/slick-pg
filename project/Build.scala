b3d268a5efbfed299588b7fce1a72e0243298a90
Minglei Tu <tmlneu@gmail.com>
2015-02-28 07:15:00 +0800

    jump to v0.8.3

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object SlickPgBuild extends Build {
     organizationName := "slick-pg",
     organization := "com.github.tminglei",
     name := "slick-pg",
-    version := "0.8.2",
+    version := "0.8.3",
 
     scalaVersion := "2.11.5",
     crossScalaVersions := Seq("2.11.5", "2.10.4"),
