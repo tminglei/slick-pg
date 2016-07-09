e8a05a767ff57faccb0964e5643822b82de97884
Minglei Tu <tmlneu@gmail.com>
2016-07-09 08:46:52 +0800

    revert wrongly submitted changes

--- core/src/main/scala/com/github/tminglei/slickpg/utils/package.scala
+++ core/src/main/scala/com/github/tminglei/slickpg/utils/package.scala
@@ -2,6 +2,6 @@ package com.github.tminglei.slickpg
 
 package object utils {
   // added to support testing, shouldn't be used externally
-  val dbUrl = "jdbc:postgresql://172.17.0.7/test?user=postgres&password=postgres"
+  val dbUrl = "jdbc:postgresql://localhost/test?user=postgres"
 
 }
