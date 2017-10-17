4d6dc1ead7ba2b285161a442226ad538c9fbce46
tminglei <tmlneu@gmail.com>
2017-10-17 16:43:36 +0800

    Remove all  u0000 characters in encoded json

--- addons/play-json/src/main/scala/com/github/tminglei/slickpg/PgPlayJsonSupport.scala
+++ addons/play-json/src/main/scala/com/github/tminglei/slickpg/PgPlayJsonSupport.scala
@@ -17,7 +17,7 @@ trait PgPlayJsonSupport extends json.PgJsonExtensions with utils.PgCommonJdbcTyp
       new GenericJdbcType[JsValue](
         pgjson,
         (v) => Json.parse(v),
-        (v) => Json.stringify(v),
+        (v) => Json.stringify(v).replace("\\u0000", ""),
         zero = JsNull,
         hasLiteralForm = false
       )
