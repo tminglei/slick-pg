8452ee332ffcfb00faa68656f990b0a0b88ff671
tuminglei <tmlneu@gmail.com>
2021-04-21 22:54:50 +0800

    remove unused

--- core/src/main/scala/com/github/tminglei/slickpg/utils/package.scala
+++
@@ -1,7 +0,0 @@
-package com.github.tminglei.slickpg
-
-package object utils {
-  // added to support testing, shouldn't be used externally
-  val dbUrl = "jdbc:postgresql://localhost/test?user=postgres"
-
-}
