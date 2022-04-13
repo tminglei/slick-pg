243bcc50e837972bd8dfeac0dd448125b61c79fa
Max <maxfelmos@mail.ru>
2022-04-13 14:05:42 +0300

    Fix test

--- src/test/scala/com/github/tminglei/slickpg/PgSearchSupportSuite.scala
+++ src/test/scala/com/github/tminglei/slickpg/PgSearchSupportSuite.scala
@@ -64,7 +64,7 @@ class PgSearchSupportSuite extends AnyFunSuite with PostgresContainer {
           ),
           // @>
           Tests.filter(r => plainToTsQuery(r.text, Some("english")) @> toTsQuery(r.comment, Some("english"))).sortBy(_.id).to[List].result.map(
-            r => assert(List(testRec1) === r)
+            r => assert(List(testRec1, testRec3, testRec4) === r)
           ),
           Tests.filter(r => phraseToTsQuery(r.text, Some("english")) @> toTsQuery(r.comment, Some("english"))).sortBy(_.id).to[List].result.map(
             r => assert(List(testRec1) === r)
