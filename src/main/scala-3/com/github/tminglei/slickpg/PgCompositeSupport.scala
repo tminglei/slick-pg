3d9d4b9be97169dbebf1cb1416a7d01e0c6905b1
Hugh Simpson <hsimpson@rzsoftware.com>
2023-12-06 23:39:47 +0000

    comment out methods so as not to lead to misuse

--- src/main/scala-3/com/github/tminglei/slickpg/PgCompositeSupport.scala
+++ src/main/scala-3/com/github/tminglei/slickpg/PgCompositeSupport.scala
@@ -14,28 +14,28 @@ import scala.compiletime.{error, erasedValue, summonInline}
 trait PgCompositeSupport extends utils.PgCommonJdbcTypes with array.PgArrayJdbcTypes { driver: PostgresProfile =>
 
   protected lazy val emptyMembersAsNull = true
-
-  //---
-  def createCompositeJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): GenericJdbcType[T] =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-
-  def createCompositeArrayJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): AdvancedArrayJdbcType[T] =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-
-  /// Plain SQL support
-  def nextComposite[T <: Struct](r: PositionedResult, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-  def nextCompositeArray[T <: Struct](r: PositionedResult, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-
-  def createCompositeSetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-
-  def createCompositeOptionSetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-  def createCompositeArraySetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) = {
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
-  }
-  def createCompositeOptionArraySetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
-    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//
+//  //---
+//  def createCompositeJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): GenericJdbcType[T] =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//
+//  def createCompositeArrayJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): AdvancedArrayJdbcType[T] =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//
+//  /// Plain SQL support
+//  def nextComposite[T <: Struct](r: PositionedResult, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//  def nextCompositeArray[T <: Struct](r: PositionedResult, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//
+//  def createCompositeSetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//
+//  def createCompositeOptionSetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//  def createCompositeArraySetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) = {
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
+//  }
+//  def createCompositeOptionArraySetParameter[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]) =
+//    throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
 }
